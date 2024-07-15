ee6f6ccc1185f79501a06b25eded37b1041b44bf
Rafi Ramdhani <148434299+rafiramdhani12@users.noreply.github.com>
2024-07-15 19:45:02 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://magicSearch_owner:a5dYpPijAhG1@ep-holy-poetry-a1b06c65.ap-southeast-1.aws.neon.tech/testAja?sslmode=require"
+  "use ur own db"
 );
 export const db = drizzle(sql, schema);
